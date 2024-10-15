813073b592e28fe7f53b0ec9170cb16a5a5c8b5a
AJi46 <ajilesh46@gmail.com>
2024-10-15 10:21:37 +0530

    chat also ready

--- lib/View/Pages/chat/chatscreen.dart
+++ lib/View/Pages/chat/chatscreen.dart
@@ -1,10 +1,265 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:quick_o_deals/Controller/auth/provider/chating_provider.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+
+// class ChatScreen extends StatefulWidget {
+//   final String userId;
+
+//   const ChatScreen({
+//     super.key,
+//     required this.userId,
+//   });
+
+//   @override
+//   State<ChatScreen> createState() => _ChatScreenState();
+// }
+
+// class _ChatScreenState extends State<ChatScreen> {
+//   final TextEditingController _messageController = TextEditingController();
+//   final ScrollController _scrollController = ScrollController();
+//   String? receiverName;
+//   String? receiverPhoneNumber;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       _scrollToBottom();
+//     });
+
+//     Provider.of<ChattingProvider>(context, listen: false)
+//         .markAsRead(widget.userId);
+
+//     _fetchUserDetails();
+//   }
+
+// Future<void> _fetchUserDetails() async {
+//   final userDetails = await _getUserDetails(widget.userId);
+//   if (userDetails != null) {
+//     setState(() {
+//       receiverName = userDetails['username'];
+//       receiverPhoneNumber = userDetails['phoneNumber'];
+//     });
+//   }
+// }
+
+//   Future<Map<String, dynamic>?> _getUserDetails(String userId) async {
+//     try {
+//       DocumentSnapshot<Map<String, dynamic>> snapshot =
+//           await FirebaseFirestore.instance.collection('users').doc(userId).get();
+
+//       if (snapshot.exists) {
+//         return snapshot.data();
+//       }
+//     } catch (e) {
+//       print('Error fetching user details: $e');
+//     }
+//     return null;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color.fromARGB(255, 202, 198, 198),
+//       appBar: AppBar(
+//         elevation: 0,
+//         backgroundColor: Colors.white,
+//         foregroundColor: Colors.black,
+//         title: Text(receiverName ?? "Loading..."),
+//         centerTitle: true,
+//         actions: [
+//           IconButton(
+//             onPressed: () => _makePhoneCall(),
+//             icon: const Icon(Icons.phone),
+//           ),
+//           // IconButton(
+//           //   icon: const Icon(Icons.video_call),
+//           //   onPressed: () {
+          
+//           //   },
+//           // ),
+//         ],
+//       ),
+//       body: Column(
+//         children: [
+//           const SizedBox(height: 10),
+//           Expanded(child: _buildMessageList()),
+//           _buildMessageInput(),
+//         ],
+//       ),
+//     );
+//   }
+//   Widget _buildMessageList() {
+//     return Consumer<ChattingProvider>(
+//       builder: (context, chattingProvider, _) {
+//         return StreamBuilder<QuerySnapshot>(
+//           stream: chattingProvider.getMessages(
+//               FirebaseAuth.instance.currentUser!.uid, widget.userId),
+//           builder: (context, snapshot) {
+//             if (snapshot.hasError) {
+//               return Text("Error: ${snapshot.error}");
+//             }
+
+//             if (!snapshot.hasData) {
+//               return const Center(child: CircularProgressIndicator());
+//             }
+
+//             WidgetsBinding.instance.addPostFrameCallback((_) {
+//               _scrollToBottom();
+//             });
+
+//             return ListView(
+//               controller: _scrollController,
+//               children: snapshot.data!.docs
+//                   .map((doc) => _buildMessageItem(doc))
+//                   .toList(),
+//             );
+//           },
+//         );
+//       },
+//     );
+//   }
+
+//   Widget _buildMessageItem(DocumentSnapshot doc) {
+//     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+//     bool isCurrentUser =
+//         data['senderId'] == FirebaseAuth.instance.currentUser!.uid;
+//     var alignment =
+//         isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
+
+//     return Container(
+//       alignment: alignment,
+//       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+//       child: Column(
+//         crossAxisAlignment:
+//             isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+//         children: [
+//           Container(
+//             constraints: BoxConstraints(
+//                 maxWidth: MediaQuery.of(context).size.width * 0.7),
+//             padding: const EdgeInsets.all(12.0),
+//             decoration: BoxDecoration(
+//               color: isCurrentUser ? Colors.blue[100] : Colors.grey[300],
+//               borderRadius: BorderRadius.circular(15.0),
+//               boxShadow: const [
+//                 BoxShadow(
+//                     color: Colors.black12, offset: Offset(1, 2), blurRadius: 5)
+//               ],
+//             ),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.end,
+//               children: [
+//                 Padding(
+//                   padding: const EdgeInsets.only(right: 50),
+//                   child: Text(data['message'],
+//                       style: const TextStyle(color: Colors.black)),
+//                 ),
+//                 const SizedBox(height: 5),
+//                 Text(
+//                   _formatTimestamp(data['timestamp']),
+//                   style: const TextStyle(color: Colors.grey, fontSize: 10.0),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   String _formatTimestamp(Timestamp timestamp) {
+//     DateTime date = timestamp.toDate();
+//     return "${date.hour}:${date.minute.toString().padLeft(2, '0')}";
+//   }
+
+//   Widget _buildMessageInput() {
+//     return Padding(
+//       padding: const EdgeInsets.all(10.0),
+//       child: Container(
+//         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+//         decoration: BoxDecoration(
+//           color: Colors.white,
+//           boxShadow: [
+//             BoxShadow(
+//               color: Colors.grey.withOpacity(0.5),
+//               spreadRadius: 2,
+//               blurRadius: 5,
+//               offset: const Offset(0, 3),
+//             ),
+//           ],
+//           borderRadius: BorderRadius.circular(30.0),
+//         ),
+//         child: Row(
+//           children: [
+//             Expanded(
+//               child: TextField(
+//                 controller: _messageController,
+//                 decoration: InputDecoration(
+//                   hintText: "Enter your message...",
+//                   hintStyle: TextStyle(color: Colors.grey[600]),
+//                   contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+//                   border: InputBorder.none,
+//                 ),
+//               ),
+//             ),
+//             IconButton(
+//               icon: const Icon(Icons.send, color: Colors.blue),
+//               onPressed: _sendMessage,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _scrollToBottom() {
+//     if (_scrollController.hasClients) {
+//       _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+//     }
+//   }
+
+//   void _sendMessage() async {
+//     if (_messageController.text.isNotEmpty) {
+//       await Provider.of<ChattingProvider>(context, listen: false)
+//           .sendMessage(widget.userId, _messageController.text);
+
+//       _messageController.clear();
+//       _scrollToBottom();
+//     }
+//   }
+
+//   void _makePhoneCall() async {
+//     if (receiverPhoneNumber != null) {
+//       final Uri launchUri = Uri(
+//         scheme: 'tel',
+//         path: receiverPhoneNumber,
+//       );
+//       if (await canLaunchUrl(launchUri)) {
+//         await launchUrl(launchUri);
+//       } else {
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           const SnackBar(content: Text('Could not launch phone call')),
+//         );
+//       }
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Phone number not available')),
+//       );
+//     }
+//   }
+// }
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import for date formatting
 import 'package:provider/provider.dart';
 import 'package:quick_o_deals/Controller/auth/provider/chating_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 
 class ChatScreen extends StatefulWidget {
   final String userId;
@@ -37,15 +292,15 @@ class _ChatScreenState extends State<ChatScreen> {
     _fetchUserDetails();
   }
 
-Future<void> _fetchUserDetails() async {
-  final userDetails = await _getUserDetails(widget.userId);
-  if (userDetails != null) {
-    setState(() {
-      receiverName = userDetails['username'];
-      receiverPhoneNumber = userDetails['phoneNumber'];
-    });
+  Future<void> _fetchUserDetails() async {
+    final userDetails = await _getUserDetails(widget.userId);
+    if (userDetails != null) {
+      setState(() {
+        receiverName = userDetails['username'];
+        receiverPhoneNumber = userDetails['phoneNumber'];
+      });
+    }
   }
-}
 
   Future<Map<String, dynamic>?> _getUserDetails(String userId) async {
     try {
@@ -76,12 +331,6 @@ Future<void> _fetchUserDetails() async {
             onPressed: () => _makePhoneCall(),
             icon: const Icon(Icons.phone),
           ),
-          // IconButton(
-          //   icon: const Icon(Icons.video_call),
-          //   onPressed: () {
-          
-          //   },
-          // ),
         ],
       ),
       body: Column(
@@ -93,6 +342,7 @@ Future<void> _fetchUserDetails() async {
       ),
     );
   }
+
   Widget _buildMessageList() {
     return Consumer<ChattingProvider>(
       builder: (context, chattingProvider, _) {
@@ -112,17 +362,65 @@ Future<void> _fetchUserDetails() async {
               _scrollToBottom();
             });
 
+            // Group messages by date
+            final messages = snapshot.data!.docs;
+            if (messages.isEmpty) {
+              return const Center(child: Text("No messages"));
+            }
+
+            List<Widget> messageWidgets = [];
+            DateTime? lastMessageDate;
+
+            for (var doc in messages) {
+              Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+              Timestamp timestamp = data['timestamp'];
+              DateTime messageDate = timestamp.toDate();
+
+              // Check if we need to add a new date header
+              if (lastMessageDate == null ||
+                  !isSameDate(lastMessageDate, messageDate)) {
+                lastMessageDate = messageDate;
+                messageWidgets.add(_buildDateHeader(messageDate));
+              }
+
+              messageWidgets.add(_buildMessageItem(doc));
+            }
+
             return ListView(
               controller: _scrollController,
-              children: snapshot.data!.docs
-                  .map((doc) => _buildMessageItem(doc))
-                  .toList(),
+              children: messageWidgets,
             );
           },
         );
       },
     );
   }
+
+  bool isSameDate(DateTime date1, DateTime date2) {
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
+  }
+
+ Widget _buildDateHeader(DateTime date) {
+  // Get today's date without time
+  DateTime today = DateTime.now();
+  bool isToday = date.year == today.year &&
+      date.month == today.month &&
+      date.day == today.day;
+
+  String headerText = isToday ? "Today" : DateFormat('MMM dd, yyyy').format(date);
+
+  return Container(
+    padding: const EdgeInsets.symmetric(vertical: 10),
+    alignment: Alignment.center,
+    child: Text(
+      headerText,
+      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+    ),
+  );
+}
+
 
   Widget _buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
@@ -170,6 +468,7 @@ Future<void> _fetchUserDetails() async {
       ),
     );
   }
+
 
   String _formatTimestamp(Timestamp timestamp) {
     DateTime date = timestamp.toDate();
